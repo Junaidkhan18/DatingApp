3c287d9a72dd9f48f4089a958691e1e912213754
Junaid Khan <junaid.khan@graymath.com>
2020-06-12 19:09:33 +0500

    Lecture six

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators'
+import {map} from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { from } from 'rxjs';
 
 @Injectable({
@@ -8,6 +9,9 @@ import { from } from 'rxjs';
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
+
 constructor(private http: HttpClient) { }
 login(model: any){
    return this.http.post(this.baseUrl + 'login', model).pipe(
@@ -15,6 +19,8 @@ login(model: any){
        const user = response;
        if (user) {
         localStorage.setItem('token', user.token);
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        console.log(this.decodedToken);
       }
      })
    );
@@ -22,5 +28,10 @@ login(model: any){
 
 register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
+}
+
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
 }
 }
